8177e52a9062f207065294ea3645dea583348d4e
jooshhart <jooshhart@gmail.com>
2024-07-05 11:58:22 -0600

    Implemented backend server and frontend client and the running of both at the same time

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import {Container} from 'react-bootstrap';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import {Outlet} from 'react-router-dom';
+import React from 'react';
 
 const App = () => {
   return (
-    <h1>
-      Welcome To ProShop
-    </h1>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
