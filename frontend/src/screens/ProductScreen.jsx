1a37e3173ba8d619cf954d4775e44eecf9ec9904
jooshhart <jooshhart@gmail.com>
2024-07-07 17:36:01 -0600

    y

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,30 +1,29 @@
-import {useState, useEffect} from 'react'
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import axios from 'axios';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import {useGetProductDetailsQuery} from '../slices/productsApiSlice'
 
 const ProductScreen = () => {
-    const [product, setProduct] = useState([]);
-
     const {id: productId} = useParams();
 
-    useEffect(() => {
-        const fetchProduct = async () => {
-        const {data} = await axios.get(`/api/products/${productId}`)
-        setProduct(data)
-        }
-        fetchProduct()
-    }, [productId])
+    const {data: product, isLoading, error} = useGetProductDetailsQuery(productId);
 
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
         Go Back
       </Link>
-      <Row>
+
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error?.data?.message || error.error}</Message>
+      ) : (
+        <Row>
         <Col md={5}>
             <Image src={product.image} alt={product.name} fluid />
         </Col>
@@ -71,6 +70,7 @@ const ProductScreen = () => {
             </Card>
         </Col>
       </Row>
+      )}
     </>
   )
 }
